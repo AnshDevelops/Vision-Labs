3be5bdc357bffdf108029ae4d4aae6e55348e810
Ansh Singhal <152869575+AnshDevelops@users.noreply.github.com>
2024-09-04 08:37:37 +0700

    get number of files

---
+++ utils.py
@@ -0,0 +1,13 @@
+import os
+import matplotlib.pyplot as plt
+from typing import Tuple, List, Dict
+
+
+def get_num_files(path):
+    """
+    Walks through a directory and prints its contents
+    :param path: Path to directory.
+    """
+
+    for path, _, filenames in os.walk(path):
+        print(f"There are {len(filenames)} images in '{path}'.")
